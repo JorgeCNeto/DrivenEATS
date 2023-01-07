4aaba21ba74b66c38988b903754c541718d9b8a5
Jorge Neto <jorge.c.d.s.neto@gmail.com>
2023-01-07 13:53:02 -0300

    confirmação do pedido e mudança no script

--- script.js
+++ script.js
@@ -1,54 +1,105 @@
 let comida;
-let comidavalor;
-function selecionarcomida(comidaselecionada){
-    const comida = document.querySelector('.opcoescomida .selecionado');
-    console.log(comida);
-    if (comida !== null){
+let ComidaNome;
+let ComidaValor;
+
+function desmarcacaoAnterior(seletor){
+    
+    const caixaSelecionada = document.querySelector(`${seletor} .selecionado`);
+
+   
+    if (caixaSelecionada !== null){
         
-        comida.classList.remove('selecionado');
+        caixaSelecionada.classList.remove('selecionado');
     }
+}
+
+
+function selecionarcomida(comidaselecionada){
+
+    desmarcacaoAnterior('.opcoescomida');
+    
     comidaselecionada.classList.add('selecionado');
-    comidavalor.
+
+    comida = comidaselecionada.innerHTML;
+    console.log(comida);
+
+    const alimentos = document.querySelectorAll('alimento');
+    ComidaNome = alimentos.innerHTML;
+    console.log(ComidaNome);
+    
+    
+    const ComidaValor = document.getElementsByClassName('preco');
+    
+    console.log(ComidaValor);
+
     verificadordecompra();
 }
+
+
 
 let bebida;
+
 function selecionarbebida(bebidaselecionada){
-    const bebida = document.querySelector('.opcoesbebida .selecionado');
-    console.log(bebida);
-    if (bebida !== null){
-        
-        bebida.classList.remove('selecionado');
-    }
+
+    desmarcacaoAnterior('.opcoesbebida');
+
     bebidaselecionada.classList.add('selecionado');
+
+    bebida = bebidaselecionada.innerHTML;
+
     verificadordecompra();
+
+}
+let sobremesa;
+
+function selecionarsobremesa(sobremesaselecionada){
+
+    desmarcacaoAnterior('.opcoessobremesa');
+
+    sobremesaselecionada.classList.add('selecionado');
+
+    sobremesa = sobremesaselecionada.innerHTML;
+
+    verificadordecompra();
+
 }
 
-let sobremesa;
-function selecionarsobremesa(sobremesaselecionada){
-    const sobremesa = document.querySelector('.opcoessobremesa .selecionado');
-    console.log(sobremesa);
-    if (sobremesa !== null){
-        
-        sobremesa.classList.remove('selecionado');
+function continuar(){
+
+    if ( comida !== undefined){
+        if ( bebida !== undefined ){
+            if ( sobremesa !== undefined ) {
+             
+            
+            // ocultar o painel de seleção de jogo e qtde de jogadores
+            //const painel = document.querySelector('.selecionar-jogo');
+            //painel.classList.add('escondido');
+            //document.querySelector('.selecionar-jogo').classList.add('escondido');
+
+            // exibir o painel de carregando o jogo 
+           // const overlay = document.querySelector('.loading-overlay');
+            //overlay.classList.remove('escondido');
+
+            // alterar a mensagem com o jogo selecionado e qtde de joga. selecionado
+           // const mensagem = document.querySelector('.mensagem-iniciando');
+            //mensagem.innerHTML = "INICIANDO POKER 100 com o jogo "+jogo+" e Qtde de Jogadores "+qtdeJogadores;
+           // mensagem.innerHTML = `INICIANDO POKER 100 com o jogo ${jogo} e Qtde de Jogadores ${qtdeJogadores}`;
+            }
+        }
     }
-    sobremesaselecionada.classList.add('selecionado');
-    verificadordecompra();
 }
 
 function verificadordecompra(){
-    if (comida !== undefined) {
-        if(bebida !== undefined){
-            if(sobremesa !== undefined){
-                console.log("funcionou");
-                const fecharpedido = document.querySelector('');
-                fecharpedido.classList.add('');
-                fecharpedido.innerHTML = "Fechar pedido";
-            }
-        }
-        
+   
+    if ( comida !== undefined ){
+        if ( bebida !== undefined){
+            if ( sobremesa !== undefined ) {
+                
+            const botaoContinuar = document.querySelector('.confirm');
+            botaoContinuar.classList.add('confirm-pronto');
+            botaoContinuar.innerHTML = "Fechar pedido";
+             }
+        }    
     }
-
-
 }
 
